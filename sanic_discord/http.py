65f0eadf2e89be137c574ffaf850f44492c0edc4
mc_fdc <45692264+tuna2134@users.noreply.github.com>
2022-03-01 12:03:46 +0900

    Update http.py

--- sanic_discord/http.py
+++ sanic_discord/http.py
@@ -7,12 +7,13 @@ class ApiError(Exception):
     pass
 
 class HttpClient:
+    Baseurl = "https://discord.com/api/v9"
     def __init__(self):
         self.session = aiohttp.ClientSession()
         
     async def request(self, method, path, *args, **kwargs):
         for t in range(5):
-            async with self.session.request(method, path, *args, **kwargs) as r:
+            async with self.session.request(method, self.Baseurl + path, *args, **kwargs) as r:
                 if r.status == 429:
                     data = await r.json()
                     if data["global"] is True:
