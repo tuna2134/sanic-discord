1c0882e68d95c23c253acc1c0ccbabf9cedf09f4
mc_fdc <45692264+tuna2134@users.noreply.github.com>
2022-03-01 12:12:18 +0900

    Update config.py

--- sanic_discord/config.py
+++ sanic_discord/config.py
@@ -1,4 +1,7 @@
 class Config:
-    def __init__(self, secret, id):
+    def __init__(self, secret, id, login_url, callback_url, redirect_url):
         self.secret = secret
         self.id = id
+        self.login_url = login_url
+        self.callback_url = callback_url
+        self.redirect_url = redirect_url
