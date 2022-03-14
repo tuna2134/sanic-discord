571ba6cfbae85ef86bc39f2293492ec91ef86873
mc_fdc <45692264+tuna2134@users.noreply.github.com>
2022-03-14 15:40:25 +0900

    Update client.py

--- sanic_discord/client.py
+++ sanic_discord/client.py
@@ -29,14 +29,22 @@ class Client:
         res.cookies["token"] = data["access_token"]
         return res
 
-    def oauth2_required(self):
+    def require():
         def decorator(f):
             @wraps(f)
             async def function(request, *args, **kwargs):
                 token = request.cookies.get("token")
+                if token is None:
+                    return redirect(self.login_url)
                 headers = {
                     "Authorization": "Bearer {}".format(token)
                 }
-                return await self.http.request("/users/@me", headers=headers)
+                async with ClientSession() as session:
+                    async with session.get("https://discord.com/api/v9/users/@me", headers=headers) as r:
+                        data = await r.json()
+                if data.get("id"):
+                    return await f(request, data, *args, **kwargs)
+                else:
+                    return redirect(self.login_url)
             return function
         return decorator
