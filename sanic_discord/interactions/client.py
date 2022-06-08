3a365a23c8c89e0ee9822c4f47fabffb3e04bcd9
mc_fdc <45692264+tuna2134@users.noreply.github.com>
2022-06-08 19:31:04 +0900

    Update client.py

--- sanic_discord/interactions/client.py
+++ sanic_discord/interactions/client.py
@@ -1,9 +1,20 @@
 from nacl.signing import VerifyKey
 from nacl.exceptions import BadSignatureError
-from sanic import Sanic
+from sanic import Sanic, Request
 from .http import HttpClient
 
 
 class InteractionClient:
-    def __init__(self, app: Sanic):
+    def __init__(self, app: Sanic, *, public_key: str):
         self.app = app
+        self.verifykey = VerifyKey(bytes.fromhex(publickey))
+        
+    async def interaction(self, request: Request):
+        signature = request.headers.get("x-signature-ed25519")
+        timestamp = request.headers.get("x-signature-timestamp")
+        try:
+            self.verify_key.verify(f'{timestamp}{request.body.decode()}'.encode(), bytes.fromhex(signature))
+        except BadSignatureError:
+            return text("invalid request signature", status=401)
+        else:
+            pass
