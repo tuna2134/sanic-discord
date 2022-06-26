8be3176d7b0da76fbaf85d6c5847a46c54e4fe65
mc_fdc <Masato.04.11.2007@gmail.com>
2022-06-26 08:26:48 +0000

    ok

--- sanic_discord/oauth/__init__.py
+++ sanic_discord/oauth/__init__.py
@@ -2,5 +2,5 @@
 .. include:: ./README.md
 """
 from .oauth import Oauth2
-from .errors import OauthException
+from .errors import OauthException, HttpException, NotFoundException
 from .access_token import AccessToken, AccessTokenType
