6f42d402cd46ef8221827738667ba3829ec29d88
mc_fdc <Masato.04.11.2007@gmail.com>
2022-06-30 04:55:12 +0000

    fix いろいろ

--- sanic_discord/oauth/blueprints.py
+++ sanic_discord/oauth/blueprints.py
@@ -5,14 +5,14 @@ from functools import wraps
 from .errors import StateError
 
 
-def exchange_code(state: bool = False) -> callable:
+def exchange_code(*args, **kwargs) -> callable:
     """
     Exchanges a code for an access token.
     """
     def decorator(func):
         @wraps(func)
-        async def wrapper(request: Request, *args, **kwargs) -> HTTPResponse:
-            results = (await request.app.ctx.oauth2._exchange_code(request, state)) + args
-            return await func(request, *results, **kwargs)
+        async def wrapper(request: Request, *args_wrapper, **kwargs_wrapper) -> HTTPResponse:
+            results = (await request.app.ctx.oauth2._exchange_code(request, *args, **kwargs)) + args_wrapper
+            return await func(request, *results, **kwargs_wrapper)
         return wrapper
     return decorator
