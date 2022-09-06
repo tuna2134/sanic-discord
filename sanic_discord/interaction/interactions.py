65404d6c47378a1eee2a69a080803162cb578a59
mc_fdc <Masato.04.11.2007@gmail.com>
2022-09-06 17:14:50 +0900

    Update interactions.py

--- sanic_discord/interaction/interactions.py
+++ sanic_discord/interaction/interactions.py
@@ -1,1 +1,12 @@
+from sanic import response
 
+from typing import TypedDict
+
+
+class InteractionType(TypedDict):
+    id: int
+    application_id: int
+
+class Interaction:
+    def __init__(self, data: InteractionType):
+        pass
