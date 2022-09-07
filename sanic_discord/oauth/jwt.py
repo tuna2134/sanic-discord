f9d301b46ab174251303befa88d3c79a1e191029
mc_fdc <Masato.04.11.2007@gmail.com>
2022-09-07 10:40:09 +0900

    Update jwt.py

--- sanic_discord/oauth/jwt.py
+++ sanic_discord/oauth/jwt.py
@@ -1,7 +1,14 @@
 from .oauth import Oauth
 
+frim sanic import Sanic
+from sanic_jwt import initialize
+from sanic_jwt.exceptions import AuthenticationFailed
+
+from typing import TypedDict
+
 
 class OauthJWT(Oauth):
 
-    def __call__(self, request, *args, **kwargs):
-        pass
+    def __init__(self, app: Sanic, options: dict, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        initialize()
